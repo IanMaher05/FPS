99aaede3c5e829afdf4b81f3437bf5ae5f231f30
MUKILTEO\1714222 <1714222@mukilteo.wednet.edu>
2023-10-24 08:40:08 -0700

    YAYYYY I did iT

---
+++ Assets/Skills.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Skills", order = 1)]
+public class Skills : ScriptableObject
+{
+    public string SkillName;
+    public int SkillDamage;
+    public int SkillDefense;
+
+    
+}
